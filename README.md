58a33e31a31f243352ebada1d7de63fceaf422a1
RominaGonella <rominagonfur@gmail.com>
2022-08-03 19:55:53 +0000

    versión final

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Template for Machine Learning projects
+# Resumen del proceso
+
+1. La tarea consiste en crear un cluster de 6 grupos a partir de 3 variables de un dataset, utilizando K-means.
+2. El dataset original contiene 20640 alojamientos, y para cada uno se dispone de 9 variables. Para crear el cluster se utilizan solamente las coordenadas (longitud y latitud) y la variable 'MedInc'.
+3. Se crea el cluster utilizando la función KMeans de sklearn, con cantidad de clusters igual a 6, como se pide en la letra del ejercicio. Se etiquetan las observaciones en la variable creada 'cluster'.
+4. Finalmente, se grafican los datos identificando con distintos colores a los distintos grupos.
--- src/app.py
+++ src/app.py
@@ -1,4 +1,29 @@
-from utils import db_connect
-engine = db_connect()
+# K-means Project Tutorial
 
-# your code here
+# se importan librerías
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import seaborn as sns
+
+# cargo datos
+df_raw = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv')
+
+# guardo el dataset original
+df_raw.to_csv('data/raw/datos_originales.csv', index = False)
+
+# se crea nuevo data frame sólo con columnas de interés
+df = df_raw[['Latitude', 'Longitude', 'MedInc']]
+
+# ajusto modelo K-mean al dataset
+clus = KMeans(n_clusters = 6, random_state = 308)
+clus.fit(df)
+
+# predicción de todo el dataset
+pred = clus.predict(df)
+
+# creo variable cluster
+df['cluster'] = pd.Categorical(pred)
+
+# guardo el dataset con el cluster
+df.to_csv('data/processed/datos_finales.csv', index = False)
